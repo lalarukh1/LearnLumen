59b694dc1588af472bdfa779ecad81a79deb811b
lalarukh.1992@outlook.com <35193027+lalarukh1@users.noreply.github.com>
2019-09-27 15:36:51 +0100

    Added authorization with Auth()

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,6 +10,51 @@ class TestController extends Controller
 
     public function showAll()
     {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://dev-p7h-jh1x.auth0.com/oauth/token",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{\"client_id\":\"qNF6lho5rKt7f2rWKCXFwXP89JA6wQFc\",\"client_secret\":\"0cHXwxcEb0WUP-LCw8PBJ0HhmjjBwayr-MlTdQHtzJY5rp3cLxgMIHnPafxbM8Z8\",\"audience\":\"test\",\"grant_type\":\"client_credentials\"}",
+            CURLOPT_HTTPHEADER => array(
+                "content-type: application/json"
+            ),
+        ));
+
+        $token = curl_exec($curl);
+        $err = curl_error($curl);
+
+        $token = json_decode($token, true);
+        $token = $token['access_token'];
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "http://localhost:8000/api/test",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "authorization: Bearer " . $token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        echo $response;
+    }
         return response()->json(Test::all());
     }
 
---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#0E3764">
+</head>
+<body>
+<div>
+  {{ $response }}
+
+</div>
+</body>
+</html>
